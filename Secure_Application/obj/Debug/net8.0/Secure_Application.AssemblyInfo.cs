e0860009dcffe1eeaf788288041aacd08a710e92
SQU23001303 <SQU23001303@btc.ac.uk>
2026-05-17 11:15:40 +0100

    Had t delete the database and restart

--- Secure_Application/obj/Debug/net8.0/Secure_Application.AssemblyInfo.cs
+++ Secure_Application/obj/Debug/net8.0/Secure_Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Secure_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae956e24776232d560b3da5822bfcd967a15f195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109c8bb2c7ec08d7adbdc00ed86c22327de19366")]
 [assembly: System.Reflection.AssemblyProductAttribute("Secure_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Secure_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
